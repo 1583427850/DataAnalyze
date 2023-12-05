b44aacb07302641529cea8f7cccf11d18ac53f45
lin <1583427850@qq.com>
2023-12-05 14:38:33 +0800

    init

---
+++ src/test/java/com/bigdata/dataanalyze/DataAnalyzeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigdata.dataanalyze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAnalyzeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
